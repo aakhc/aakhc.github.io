c0a7f647fdb4587d4b4498d266fe192f5a7196cb
aaron <aaron@kinghub.cc>
2021-08-11 17:04:53 +0800

    update: misc

--- spine-demo3/src/settings.js
+++ spine-demo3/src/settings.js
@@ -1,1 +1,30 @@
-window._CCSettings={platform:"web-mobile",groupList:["default","ui","bg"],collisionMatrix:[[false],[false,false],[false,false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/SpineDemo.fire",orientation:"",jsList:[]};
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default",
+        "ui",
+        "bg"
+    ],
+    collisionMatrix: [
+        [
+            false
+        ],
+        [
+            false,
+            false
+        ],
+        [
+            false,
+            false,
+            false
+        ]
+    ],
+    hasResourcesBundle: false,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/scene/SpineDemo.fire",
+    orientation: "",
+    debug: true,
+    jsList: []
+};
