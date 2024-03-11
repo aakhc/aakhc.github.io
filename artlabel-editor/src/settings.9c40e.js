0874ba2a9fd325f875c43b27fa0cb2c0c84f1691
aaron <aaron@kinghub.cc>
2024-03-11 09:46:48 +0800

    update: misc

---
+++ artlabel-editor/src/settings.9c40e.js
@@ -0,0 +1,28 @@
+window._CCSettings = {
+    platform: "web-desktop",
+    groupList: [
+        "default",
+        "ui"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ],
+        [
+            false,
+            false
+        ]
+    ],
+    hasResourcesBundle: false,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/scene/ArtLabelEditor.fire",
+    orientation: "",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "93771",
+        main: "1b78f"
+    }
+};
