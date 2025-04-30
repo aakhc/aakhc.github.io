9f8b9946df51f5be117f5bf5cb48a2c0159b2e53
cooper <cooper.huang@kinghub.cc>
2025-04-30 17:19:37 +0800

    update: artlabel-editor

--- artlabel-editor/application.c4cca.js
+++ artlabel-editor/application.c4cca.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.6a8dc.json';
+          this.settingsPath = 'src/settings.ffb2e.json';
           this.showFPS = true;
         }
         _createClass(Application, [{
--- artlabel-editor/index.408aa.js
+++ artlabel-editor/index.408aa.js
@@ -1,4 +1,4 @@
-System.register(["./application.0823d.js"], function (_export, _context) {
+System.register(["./application.c4cca.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
